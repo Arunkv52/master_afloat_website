83fefdfcd4f8809eb1bf3e3987f4ad61ea601ecd
Arunkv52 <arunkbva5@gmail.com>
2025-10-07 18:08:57 +0530

    new file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,17 +3,24 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const Contact = () => {
-
   //contact value will stored in a states
 
-  const [fullname,setFullname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone,setPhone] = useState('');
-  const [company,setCompany] = useState('');
-  const [message,setMessage] = useState('');
+  const [fullname, setFullname] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [company, setCompany] = useState('')
+  const [message, setMessage] = useState('')
 
- 
-  
+  function userData() {
+    event.preventDefault()
+
+    console.log('fullname :', fullname)
+    console.log('email :', email)
+    console.log('phone :', phone)
+    console.log('company :', company)
+    console.log('message :', message)
+  }
+
   return (
     <>
       <Navbar />
@@ -27,7 +34,7 @@ const Contact = () => {
           </p>
         </div>
       </div>
-      
+
       <div className='bg-white text-black md:py-20 py-10 md:px-30 px-5 md:flex justify-between items-start gap-5'>
         <div className='md:w-1/2 w-full'>
           <p>Discuss the Project</p>
@@ -40,15 +47,17 @@ const Contact = () => {
             </h2>
 
             {/* Form */}
-            <form className='space-y-6'>
+            <form className='space-y-6' onSubmit={userData}>
               <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {/* Full Name */}
                 <input
                   type='text'
                   value={fullname}
-                  onChange={(event) => setFullname(event.target.value)}
+                  onChange={event => setFullname(event.target.value)}
                   placeholder='Full name *'
                   className='w-full p-4 bg-gray-100 rounded-lg outline-none'
+                  required
+                  maxLength={20}
                 />
 
                 {/* Email */}
@@ -56,8 +65,10 @@ const Contact = () => {
                   type='email'
                   placeholder='Email *'
                   value={email}
-                  
+                  onChange={event => setEmail(event.target.value)}
                   className='w-full p-4 bg-gray-100 rounded-lg outline-none'
+                  required
+                  maxLength={20}
                 />
               </div>
 
@@ -68,6 +79,10 @@ const Contact = () => {
                     type='tel'
                     placeholder='Phone number *'
                     className='w-full p-4 bg-gray-100 rounded-r-lg outline-none'
+                    value={company}
+                    onChange={event => setCompany(event.target.value)}
+                    required
+                    maxLength={20}
                   />
                 </div>
 
@@ -76,6 +91,10 @@ const Contact = () => {
                   type='text'
                   placeholder='Company'
                   className='w-full p-4 bg-gray-100 rounded-lg outline-none'
+                  value={phone}
+                  onChange={event => setPhone(event.target.value)}
+                  required
+                  maxLength={20}
                 />
               </div>
 
@@ -84,25 +103,16 @@ const Contact = () => {
                 placeholder='Message'
                 rows='4'
                 className='w-full p-4 bg-gray-100 rounded-lg outline-none'
+                value={message}
+                onChange={event => setMessage(event.target.value)}
+                required
+                maxLength={20}
               ></textarea>
-
-              {/* Privacy Notice */}
-              <div className='flex items-start space-x-2 text-sm text-gray-600'>
-                <input type='checkbox' className='mt-1' />
-                <p>
-                  We will add your info to our CRM for contacting you regarding
-                  your request. For more info please consult our{' '}
-                  <span className='text-orange-500 cursor-pointer'>
-                    privacy policy
-                  </span>
-                  .
-                </p>
-              </div>
 
               {/* Submit Button */}
               <button
                 type='submit'
-                className='px-6 py-3 bg-gradient-to-r from-orange-400 to-orange-600 text-white font-semibold rounded-full hover:opacity-90 transition'
+                className='cursor-pointer px-6 py-3 bg-gradient-to-r from-orange-400 to-orange-600 text-white font-semibold rounded-full hover:opacity-90 transition'
               >
                 Send Message
               </button>
@@ -122,13 +132,13 @@ const Contact = () => {
                 No. 7 Soon Lee Street, #02-16 I Space, Singapore 627608.
               </p>
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.429087096093!2d103.74929807395968!3d1.3105118986770532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x100404540f3bcaad%3A0xd41e4733d7f2f13b!2sMaster%20Marine%20(Pte)%20Ltd!5e1!3m2!1sen!2sin!4v1759133138365!5m2!1sen!2sin"
-                width="100%"
-                height="200"
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                className="rounded-lg shadow-md"
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.429087096093!2d103.74929807395968!3d1.3105118986770532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x100404540f3bcaad%3A0xd41e4733d7f2f13b!2sMaster%20Marine%20(Pte)%20Ltd!5e1!3m2!1sen!2sin!4v1759133138365!5m2!1sen!2sin'
+                width='100%'
+                height='200'
+                allowFullScreen=''
+                loading='lazy'
+                referrerPolicy='no-referrer-when-downgrade'
+                className='rounded-lg shadow-md'
               ></iframe>
             </div>
             <div className='pb-1'>
